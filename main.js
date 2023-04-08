febc14a4363528a0800128d51ed7534277a31883
Hùng <hungvu300899@gmail.com>
2023-04-09 01:11:25 +0700

    first commit

--- main.js
+++ main.js
@@ -4,21 +4,20 @@ function Header(){
     return (
         <div class="header">
             <div class="logo">
-                <img src="images/icon.png" alt=""></img>
-                <h1>NCC</h1>
+                <img src="images/logoNCC.png" alt=""></img>
             </div>
-            <p>We do <span>it</span> with passion</p>
         </div>
     )
 }
 function Introduce(){
     return (
         <div class="introduce">
-            <strong>Chúng tôi là ai?</strong>
+            <strong>Lorem ipsum dolor sit asmet?</strong>
             <p>
-                NCC là một công ty chuyên cung cấp dịch vụ gia công phần mềm công nghệ thông tin. 
-                Được thành lập từ năm 2014 và chúng tôi đã nhanh chóng khẳng định vị thế của mình trên thị trường, 
-                hiện đang là một trong những doanh nghiệp công nghệ thông tin uy tín tại Việt Nam.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique consequat placerat. Vestibulum auctor pellentesque sem, 
+                eu posuere erat hendrerit quis. Maecenas vel consequat turpis. Nam facilisis, ligula in mattis sodales, augue justo tristique nulla, 
+                sed lacinia ante eros ut mi. Morbi vitae diam augue. Aliquam vel mauris a nibh auctor commodo. Praesent et nisi eu justo eleifend convallis. 
+                Quisque suscipit maximus vestibulum. Phasellus congue mollis orci, sit amet luctus augue tristique eu. Donec vulputate odio neque, sed semper turpis pellentesque a.
             </p>
         </div>
     )
@@ -27,18 +26,18 @@ function Courses(props){
     const courses = [ 
         {
         title:'Lorem ipsum dolor sit amet',
-        src:'../images/icon_css.avif',
-        description: 'HTML là từ viết tắt của Hypertext Markup Language, là sự kết hợp của Hypertext và Markup, hay còn được gọi là ngôn ngữ siêu văn bản. HTML có chức năng giúp người dùng xây dựng và cấu trúc các phần trong trang web hoặc ứng dụng, thường được sử dụng trong phân chia các đoạn văn, heading, link, blockquotes,…' 
+        src:'../images/css-icon.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.' 
         },
         {
         title:'Lorem ipsum dolor sit amet',
-        src:'../images/icon_css.avif',
-        description: 'HTML là từ viết tắt của Hypertext Markup Language, là sự kết hợp của Hypertext và Markup, hay còn được gọi là ngôn ngữ siêu văn bản. HTML có chức năng giúp người dùng xây dựng và cấu trúc các phần trong trang web hoặc ứng dụng, thường được sử dụng trong phân chia các đoạn văn, heading, link, blockquotes,…' 
+        src:'../images/html-icon.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.' 
         },
         {
         title:'Lorem ipsum dolor sit amet',
-        src:'../images/icon_css.avif',
-        description: 'HTML là từ viết tắt của Hypertext Markup Language, là sự kết hợp của Hypertext và Markup, hay còn được gọi là ngôn ngữ siêu văn bản. HTML có chức năng giúp người dùng xây dựng và cấu trúc các phần trong trang web hoặc ứng dụng, thường được sử dụng trong phân chia các đoạn văn, heading, link, blockquotes,…' 
+        src:'../images/url-icon.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.' 
         }
     ]
     return(
@@ -57,7 +56,7 @@ function Courses(props){
 function Footer(){
     return(
         <div class="footer">
-            <p>Copyright 2021</p>
+            <p>Copyright © 2021</p>
         </div>
     )
 }
